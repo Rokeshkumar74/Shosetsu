e810fc4e68e15419f502bd27135bc51f3e20c007
Rokeshkumar74 <rokeshkumar74@gmail.com>
2025-07-01 09:35:23 +0530

    Add files via upload

---
+++ extras/index.js
@@ -0,0 +1,26 @@
+// const hellping = require("./hellping");
+const cclaw = require("./cclaw");
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({
+        "contents": [
+            // {
+            //     "id": "hellping",
+            //     "name": "Hellping",
+            //     "host": "https://hellping.org"
+            // },
+            {
+                "id": "cclaw",
+                "name": "CClaw Translations",
+                "host": "https://cclawtranslations.home.blog"
+            }
+        ]
+    })
+})
+// router.use("/hellping", hellping);
+router.use("/cclaw", cclaw);
+
+module.exports = router;
